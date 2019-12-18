7c97b12886f57bb058284c76921f4a4964e12b2d
Rajeev R Menon <rajeevrmenon97@live.com>
2019-12-19 01:06:25 +0530

    add activity stream

---
+++ activitystream/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+class Post(models.Model):
+    content = models.TextField(blank=False)
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to ='uploads/% Y/% m/% d/',default=None, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_timestamp = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    content = models.TextField(blank=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_timestamp = models.DateTimeField(auto_now=True)
+
+class Upvote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Activity(models.Model):
+    actor = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    
+    class Verb(models.TextChoices):
+        POST = 'PST', _('Post')
+        COMMENT = 'CMT', _('Comment')
+        UPVOTE = 'UVT', _('Upvote')
+
+    verb = models.CharField(max_length=3, choices=Verb.choices, blank=False)
+    comment = models.ForeignKey(Comment, default=None, blank=True, null=True, on_delete=models.CASCADE)
+    post =  models.ForeignKey(Post, default=None, blank=True, null=True, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Following(models.Model):
+    user = models.ForeignKey(User,related_name='baseuser', blank=False, on_delete=models.CASCADE)
+    target_user = models.ForeignKey(User,related_name='targeruser', blank=False, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+
